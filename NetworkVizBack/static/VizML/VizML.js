a48c28403526541b08aae0b30a5b1ac2dbfd8cc8
yishangru <yishangru@126.com>
2020-04-20 21:47:34 -0400

    front

--- NetworkVizBack/static/VizML/VizML.js
+++ NetworkVizBack/static/VizML/VizML.js
@@ -3,7 +3,10 @@ function VizML(parentBlockId) {
     var workingDIV = d3.select("#" + parentBlockId).append("div")
         .attr("id", "VizML");
 
-    // prepare the dashboard
+    this.nodeId = 0;
+    this.linkId = 0;
+    this.nodeMapping = d3.map();
+    /* prepare the dashboard */
     this.vizPanelSpan = workingDIV.append("span").attr("id", "dashBoardDiv");
     this.dashBoardSpan = workingDIV.append("span").attr("id", "vizPanelDiv");
 }
@@ -11,29 +14,51 @@ function VizML(parentBlockId) {
 VizML.prototype.initialViz = function(APIData) {
     console.log(APIData);
     // append node for sub svg 3 node in a row
-    this.linkedData
-    this.updateDashBoard(APIData);
+    this.linkedData = APIData;
+    this.updateDashBoard(this.linkedData);
+};
 
-}
+VizML.prototype.getNodeId = function() {
+    var generateId = this.nodeId;
+    this.nodeId++;
+    return generateId;
+};
+
+VizML.prototype.getLinkId = function() {
+    var generateId = this.linkId;
+    this.linkId++;
+    return generateId;
+};
 
 VizML.prototype.updateDashBoard = function(APIData) {
+    /* add node */
 
-}
+    /* event register */
+};
 
-VizML.prototype.addNode = function () {
+VizML.prototype.addNode = function (NodeInfo) {
+    var generatedNode = JSON.parse(JSON.stringify(NodeInfo)); // return a deep copy of the node
+    if (!generatedNode.hasOwnProperty("id"))
+        generatedNode["id"] = this.getNodeId();
+    if (!generateNode.hasOwnProperty("position"))
+        generatedNode["position"] = {"x": 200, "y": 200};
 
-}
+    // add to node map
+
+    // update in the global map (maintain the link)
+
+};
 
 VizML.prototype.addLink = function () {
 
-}
+};
 
 VizML.prototype.removeNode = function () {
 
-}
+};
 
 VizML.prototype.removeLink = function() {
 
-}
+};
 
 export { VizML };
